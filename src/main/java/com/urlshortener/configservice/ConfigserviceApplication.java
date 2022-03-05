588ecb2bbb71a85c444403d986fb3f2c79087c91
berkerigdir <berker.igdir@codeitlabs.com>
2022-03-05 13:31:34 +0100

    first commit

---
+++ src/main/java/com/urlshortener/configservice/ConfigserviceApplication.java
@@ -0,0 +1,14 @@
+package com.urlshortener.configservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigserviceApplication.class, args);
+	}
+}
